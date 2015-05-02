7fc4dc12b0f3f929f46919babeeea25737ec92f5
Dylan Moore <moore.dylan.e@gmail.com>
2015-05-02 08:50:40 -0700

    First commit!

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/smslack.jar clojure.main -m smslack.core
---
+++ src/smslack/middleware.clj
@@ -0,0 +1,70 @@
+(ns smslack.middleware
+  (:require [smslack.session :as session]
+            [smslack.layout :refer [*servlet-context*]]
+            [taoensso.timbre :as timbre]
+            [environ.core :refer [env]]
+            [selmer.middleware :refer [wrap-error-page]]
+            [prone.middleware :refer [wrap-exceptions]]
+            [ring.util.response :refer [redirect]]
+            [ring.middleware.defaults :refer [site-defaults wrap-defaults]]
+            [ring.middleware.session-timeout :refer [wrap-idle-session-timeout]]
+            [ring.middleware.session.memory :refer [memory-store]]
+            [ring.middleware.format :refer [wrap-restful-format]]
+            [ring.middleware.anti-forgery :refer [wrap-anti-forgery]]
+            
+            ))
+
+(defn wrap-servlet-context [handler]
+  (fn [request]
+    (binding [*servlet-context*
+              (if-let [context (:servlet-context request)]
+                ;; If we're not inside a serlvet environment
+                ;; (for example when using mock requests), then
+                ;; .getContextPath might not exist
+                (try (.getContextPath context)
+                     (catch IllegalArgumentException _ context)))]
+      (handler request))))
+
+(defn wrap-internal-error [handler]
+  (fn [req]
+    (try
+      (handler req)
+      (catch Throwable t
+        (timbre/error t)
+        {:status 500
+         :headers {"Content-Type" "text/html"}
+         :body "<body>
+                  <h1>Something very bad has happened!</h1>
+                  <p>We've dispatched a team of highly trained gnomes to take care of the problem.</p>
+                </body>"}))))
+
+(defn development-middleware [handler]
+  (if (env :dev)
+    (-> handler
+        wrap-error-page
+        wrap-exceptions)
+    handler))
+
+
+(defn wrap-csrf
+  "disables CSRF for URIs that match the specified pattern"
+  [handler pattern]
+  (let [anti-forgery-handler (wrap-anti-forgery handler)]
+    (fn [req]
+      (if (re-matches pattern (:uri req))
+        (handler req)
+        (anti-forgery-handler req)))))
+
+(defn production-middleware [handler]
+  (-> handler
+      (wrap-restful-format :formats [:json-kw :edn :transit-json :transit-msgpack])
+      (wrap-idle-session-timeout
+        {:timeout (* 60 30)
+         :timeout-response (redirect "/")})
+      (wrap-csrf #"^/api/.*")
+      (wrap-defaults
+        (-> site-defaults
+            (assoc-in [:security :anti-forgery] false)
+            (assoc-in  [:session :store] (memory-store session/mem))))
+      wrap-internal-error))
+
